112346bdcf64f73b80a939597535034cde34b4ff
Julián Martinez <julian.itsa8@gmail.com>
2024-08-12 19:17:32 +0200

    created xcode project

---
+++ GHFollowers-SwiftUI/GHFollowers_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  GHFollowers_SwiftUIApp.swift
+//  GHFollowers-SwiftUI
+//
+//  Created by Julian Martinez Cordones on 12/8/24.
+//
+
+import SwiftUI
+
+@main
+struct GHFollowers_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
